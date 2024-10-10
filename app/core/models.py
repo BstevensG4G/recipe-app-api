68f5fa5449d894dbed7bd65e4f9688ad060d3db4
Bill Stevens <g4gbill@gmail.com>
2024-10-10 13:20:52 -1000

    fixed flake8 linespace and whitespace issues

--- app/core/models.py
+++ app/core/models.py
@@ -48,6 +48,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
+
 class Recipe(models.Model):
     """Recipe object."""
     user = models.ForeignKey(
